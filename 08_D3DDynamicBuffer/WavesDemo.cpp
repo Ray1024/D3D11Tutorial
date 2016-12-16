109246b212695c80894d423964b0f27d4dd2b0e9
郭建磊 <guojianlei010@163.com>
2016-12-16 21:34:33 +0800

    add tests 06-08.
    add tests 06-08.

---
+++ 08_D3DDynamicBuffer/WavesDemo.cpp
@@ -0,0 +1,470 @@
+//***************************************************************************************
+// WavesDemo.cpp by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Demonstrates dynamic vertex buffers by performing an animated wave simulation where
+// the vertex buffers are updated every frame with the new snapshot of the wave simulation.
+//
+// Controls:
+//		Hold the left mouse button down and move the mouse to rotate.
+//      Hold the right mouse button down to zoom in and out.
+//
+//***************************************************************************************
+
+#include "../Common/D3D11App.h"
+#include "../Common/GeometryGenerator.h"
+#include "../Common/MathHelper.h"
+#include "d3dx11Effect.h"
+#include "Waves.h"
+
+struct Vertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
+
+class WavesDemo : public D3D11App
+{
+public:
+	WavesDemo(HINSTANCE hInstance);
+	~WavesDemo();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene(); 
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	float GetHeight(float x, float z)const;
+	void BuildLandGeometryBuffers();
+	void BuildWavesGeometryBuffers();
+	void BuildFX();
+	void BuildVertexLayout();
+
+private:
+	ID3D11Buffer* mLandVB;
+	ID3D11Buffer* mLandIB;
+	ID3D11Buffer* mWavesVB;
+	ID3D11Buffer* mWavesIB;
+
+	ID3DX11Effect* mFX;
+	ID3DX11EffectTechnique* mTech;
+	ID3DX11EffectMatrixVariable* mfxWorldViewProj;
+
+	ID3D11InputLayout* mInputLayout;
+
+	ID3D11RasterizerState* mWireframeRS;
+
+	// Define transformations from local spaces to world space.
+	XMFLOAT4X4 mGridWorld;
+	XMFLOAT4X4 mWavesWorld;
+
+	UINT mGridIndexCount;
+
+	Waves mWaves;
+
+	XMFLOAT4X4 mView;
+	XMFLOAT4X4 mProj;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+				   PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
+	WavesDemo theApp(hInstance);
+	
+	if( !theApp.Init() )
+		return 0;
+	
+	return theApp.Run();
+}
+ 
+
+WavesDemo::WavesDemo(HINSTANCE hInstance)
+: D3D11App(hInstance), mLandVB(0), mLandIB(0), mWavesVB(0), mWavesIB(0), mFX(0), mTech(0),
+  mfxWorldViewProj(0), mInputLayout(0), mWireframeRS(0), mGridIndexCount(0),
+  mTheta(1.5f*MathHelper::Pi), mPhi(0.1f*MathHelper::Pi), mRadius(200.0f)
+{
+	m_mainWndCaption = L"Waves Demo";
+	
+	mLastMousePos.x = 0;
+	mLastMousePos.y = 0;
+
+	XMMATRIX I = XMMatrixIdentity();
+	XMStoreFloat4x4(&mGridWorld, I);
+	XMStoreFloat4x4(&mWavesWorld, I);
+	XMStoreFloat4x4(&mView, I);
+	XMStoreFloat4x4(&mProj, I);
+}
+
+WavesDemo::~WavesDemo()
+{
+	ReleaseCOM(mLandVB);
+	ReleaseCOM(mLandIB);
+	ReleaseCOM(mWavesVB);
+	ReleaseCOM(mWavesIB);
+	ReleaseCOM(mFX);
+	ReleaseCOM(mInputLayout);
+	ReleaseCOM(mWireframeRS);
+}
+
+bool WavesDemo::Init()
+{
+	if(!D3D11App::Init())
+		return false;
+
+	mWaves.Init(200, 200, 0.8f, 0.03f, 3.25f, 0.4f);
+
+	BuildLandGeometryBuffers();
+	BuildWavesGeometryBuffers();
+	BuildFX();
+	BuildVertexLayout();
+
+	D3D11_RASTERIZER_DESC wireframeDesc;
+	ZeroMemory(&wireframeDesc, sizeof(D3D11_RASTERIZER_DESC));
+	wireframeDesc.FillMode = D3D11_FILL_WIREFRAME;
+	wireframeDesc.CullMode = D3D11_CULL_BACK;
+	wireframeDesc.FrontCounterClockwise = false;
+	wireframeDesc.DepthClipEnable = true;
+
+	HR(m_pD3DDevice->CreateRasterizerState(&wireframeDesc, &mWireframeRS));
+
+	return true;
+}
+
+void WavesDemo::OnResize()
+{
+	D3D11App::OnResize();
+
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+	XMStoreFloat4x4(&mProj, P);
+}
+
+void WavesDemo::UpdateScene(float dt)
+{
+	// Convert Spherical to Cartesian coordinates.
+	float x = mRadius*sinf(mPhi)*cosf(mTheta);
+	float z = mRadius*sinf(mPhi)*sinf(mTheta);
+	float y = mRadius*cosf(mPhi);
+
+	// Build the view matrix.
+	XMVECTOR pos    = XMVectorSet(x, y, z, 1.0f);
+	XMVECTOR target = XMVectorZero();
+	XMVECTOR up     = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMMATRIX V = XMMatrixLookAtLH(pos, target, up);
+	XMStoreFloat4x4(&mView, V);
+
+	//
+	// Every quarter second, generate a random wave.
+	//
+	static float t_base = 0.0f;
+	if( (m_timer.TotalTime() - t_base) >= 0.25f )
+	{
+		t_base += 0.25f;
+ 
+		DWORD i = 5 + rand() % 190;
+		DWORD j = 5 + rand() % 190;
+
+		float r = MathHelper::RandF(1.0f, 2.0f);
+
+		mWaves.Disturb(i, j, r);
+	}
+
+	mWaves.Update(dt);
+
+	//
+	// Update the wave vertex buffer with the new solution.
+	//
+	
+	D3D11_MAPPED_SUBRESOURCE mappedData;
+	HR(m_pD3DImmediateContext->Map(mWavesVB, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedData));
+
+	Vertex* v = reinterpret_cast<Vertex*>(mappedData.pData);
+	for(UINT i = 0; i < mWaves.VertexCount(); ++i)
+	{
+		v[i].Pos = mWaves[i];
+		v[i].Color = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+	}
+
+	m_pD3DImmediateContext->Unmap(mWavesVB, 0);
+}
+
+void WavesDemo::DrawScene()
+{
+	m_pD3DImmediateContext->ClearRenderTargetView(m_pRenderTargetView, reinterpret_cast<const float*>(&Colors::LightSteelBlue));
+	m_pD3DImmediateContext->ClearDepthStencilView(m_pDepthStencilView, D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	m_pD3DImmediateContext->IASetInputLayout(mInputLayout);
+    m_pD3DImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+ 
+	UINT stride = sizeof(Vertex);
+    UINT offset = 0;
+    
+	XMMATRIX view  = XMLoadFloat4x4(&mView);
+	XMMATRIX proj  = XMLoadFloat4x4(&mProj);
+	
+    D3DX11_TECHNIQUE_DESC techDesc;
+    mTech->GetDesc( &techDesc );
+    for(UINT p = 0; p < techDesc.Passes; ++p)
+    {
+		//
+		// Draw the land.
+		//
+		m_pD3DImmediateContext->IASetVertexBuffers(0, 1, &mLandVB, &stride, &offset);
+		m_pD3DImmediateContext->IASetIndexBuffer(mLandIB, DXGI_FORMAT_R32_UINT, 0);
+
+		XMMATRIX world = XMLoadFloat4x4(&mGridWorld);
+		XMMATRIX worldViewProj = world*view*proj;
+		mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&worldViewProj));
+		mTech->GetPassByIndex(p)->Apply(0, m_pD3DImmediateContext);
+		m_pD3DImmediateContext->DrawIndexed(mGridIndexCount, 0, 0);
+
+		//
+		// Draw the waves.
+		//
+
+		m_pD3DImmediateContext->RSSetState(mWireframeRS);
+
+		m_pD3DImmediateContext->IASetVertexBuffers(0, 1, &mWavesVB, &stride, &offset);
+		m_pD3DImmediateContext->IASetIndexBuffer(mWavesIB, DXGI_FORMAT_R32_UINT, 0);
+
+		world = XMLoadFloat4x4(&mWavesWorld);
+		worldViewProj = world*view*proj;
+		mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&worldViewProj));
+		mTech->GetPassByIndex(p)->Apply(0, m_pD3DImmediateContext);
+		m_pD3DImmediateContext->DrawIndexed(3*mWaves.TriangleCount(), 0, 0);
+
+		// Restore default.
+		m_pD3DImmediateContext->RSSetState(0);
+    }
+
+	HR(m_pSwapChain->Present(0, 0));
+}
+
+void WavesDemo::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+
+	SetCapture(m_hMainWnd);
+}
+
+void WavesDemo::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void WavesDemo::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if( (btnState & MK_LBUTTON) != 0 )
+	{
+		// Make each pixel correspond to a quarter of a degree.
+		float dx = XMConvertToRadians(0.25f*static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(0.25f*static_cast<float>(y - mLastMousePos.y));
+
+		// Update angles based on input to orbit camera around box.
+		mTheta += dx;
+		mPhi   += dy;
+
+		// Restrict the angle mPhi.
+		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi-0.1f);
+	}
+	else if( (btnState & MK_RBUTTON) != 0 )
+	{
+		// Make each pixel correspond to 0.2 unit in the scene.
+		float dx = 0.2f*static_cast<float>(x - mLastMousePos.x);
+		float dy = 0.2f*static_cast<float>(y - mLastMousePos.y);
+
+		// Update the camera radius based on input.
+		mRadius += dx - dy;
+
+		// Restrict the radius.
+		mRadius = MathHelper::Clamp(mRadius, 50.0f, 500.0f);
+	}
+
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
+
+float WavesDemo::GetHeight(float x, float z)const
+{
+	return 0.3f*( z*sinf(0.1f*x) + x*cosf(0.1f*z) );
+}
+
+void WavesDemo::BuildLandGeometryBuffers()
+{
+	GeometryGenerator::MeshData grid;
+ 
+	GeometryGenerator geoGen;
+
+	geoGen.CreateGrid(160.0f, 160.0f, 50, 50, grid);
+
+	mGridIndexCount = grid.Indices.size();
+
+	//
+	// Extract the vertex elements we are interested and apply the height function to
+	// each vertex.  In addition, color the vertices based on their height so we have
+	// sandy looking beaches, grassy low hills, and snow mountain peaks.
+	//
+
+	std::vector<Vertex> vertices(grid.Vertices.size());
+	for(size_t i = 0; i < grid.Vertices.size(); ++i)
+	{
+		XMFLOAT3 p = grid.Vertices[i].Position;
+
+		p.y = GetHeight(p.x, p.z);
+
+		vertices[i].Pos   = p;
+		
+		// Color the vertex based on its height.
+		if( p.y < -10.0f )
+		{
+			// Sandy beach color.
+			vertices[i].Color = XMFLOAT4(1.0f, 0.96f, 0.62f, 1.0f);
+		}
+		else if( p.y < 5.0f )
+		{
+			// Light yellow-green.
+			vertices[i].Color = XMFLOAT4(0.48f, 0.77f, 0.46f, 1.0f);
+		}
+		else if( p.y < 12.0f )
+		{
+			// Dark yellow-green.
+			vertices[i].Color = XMFLOAT4(0.1f, 0.48f, 0.19f, 1.0f);
+		}
+		else if( p.y < 20.0f )
+		{
+			// Dark brown.
+			vertices[i].Color = XMFLOAT4(0.45f, 0.39f, 0.34f, 1.0f);
+		}
+		else
+		{
+			// White snow.
+			vertices[i].Color = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+		}
+	}
+
+    D3D11_BUFFER_DESC vbd;
+    vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof(Vertex) * grid.Vertices.size();
+    vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    vbd.CPUAccessFlags = 0;
+    vbd.MiscFlags = 0;
+    D3D11_SUBRESOURCE_DATA vinitData;
+    vinitData.pSysMem = &vertices[0];
+    HR(m_pD3DDevice->CreateBuffer(&vbd, &vinitData, &mLandVB));
+
+	//
+	// Pack the indices of all the meshes into one index buffer.
+	//
+
+	D3D11_BUFFER_DESC ibd;
+    ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(UINT) * mGridIndexCount;
+    ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+    ibd.CPUAccessFlags = 0;
+    ibd.MiscFlags = 0;
+    D3D11_SUBRESOURCE_DATA iinitData;
+	iinitData.pSysMem = &grid.Indices[0];
+    HR(m_pD3DDevice->CreateBuffer(&ibd, &iinitData, &mLandIB));
+}
+ 
+void WavesDemo::BuildWavesGeometryBuffers()
+{
+	// Create the vertex buffer.  Note that we allocate space only, as
+	// we will be updating the data every time step of the simulation.
+
+    D3D11_BUFFER_DESC vbd;
+    vbd.Usage = D3D11_USAGE_DYNAMIC;
+	vbd.ByteWidth = sizeof(Vertex) * mWaves.VertexCount();
+    vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    vbd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+    vbd.MiscFlags = 0;
+    HR(m_pD3DDevice->CreateBuffer(&vbd, 0, &mWavesVB));
+
+
+	// Create the index buffer.  The index buffer is fixed, so we only 
+	// need to create and set once.
+
+	std::vector<UINT> indices(3*mWaves.TriangleCount()); // 3 indices per face
+
+	// Iterate over each quad.
+	UINT m = mWaves.RowCount();
+	UINT n = mWaves.ColumnCount();
+	int k = 0;
+	for(UINT i = 0; i < m-1; ++i)
+	{
+		for(DWORD j = 0; j < n-1; ++j)
+		{
+			indices[k]   = i*n+j;
+			indices[k+1] = i*n+j+1;
+			indices[k+2] = (i+1)*n+j;
+
+			indices[k+3] = (i+1)*n+j;
+			indices[k+4] = i*n+j+1;
+			indices[k+5] = (i+1)*n+j+1;
+
+			k += 6; // next quad
+		}
+	}
+
+	D3D11_BUFFER_DESC ibd;
+    ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(UINT) * indices.size();
+    ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+    ibd.CPUAccessFlags = 0;
+    ibd.MiscFlags = 0;
+    D3D11_SUBRESOURCE_DATA iinitData;
+    iinitData.pSysMem = &indices[0];
+    HR(m_pD3DDevice->CreateBuffer(&ibd, &iinitData, &mWavesIB));
+}
+
+void WavesDemo::BuildFX()
+{
+	std::ifstream fin("fx/color.fxo", std::ios::binary);
+
+	fin.seekg(0, std::ios_base::end);
+	int size = (int)fin.tellg();
+	fin.seekg(0, std::ios_base::beg);
+	std::vector<char> compiledShader(size);
+
+	fin.read(&compiledShader[0], size);
+	fin.close();
+	
+	HR(D3DX11CreateEffectFromMemory(&compiledShader[0], size, 
+		0, m_pD3DDevice, &mFX));
+
+	mTech    = mFX->GetTechniqueByName("ColorTech");
+	mfxWorldViewProj = mFX->GetVariableByName("gWorldViewProj")->AsMatrix();
+}
+
+void WavesDemo::BuildVertexLayout()
+{
+	// Create the vertex input layout.
+	D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
+	};
+
+	// Create the input layout
+    D3DX11_PASS_DESC passDesc;
+    mTech->GetPassByIndex(0)->GetDesc(&passDesc);
+	HR(m_pD3DDevice->CreateInputLayout(vertexDesc, 2, passDesc.pIAInputSignature, 
+		passDesc.IAInputSignatureSize, &mInputLayout));
+}
